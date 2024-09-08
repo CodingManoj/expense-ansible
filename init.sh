08fb1a8499787c4b369f06ece14fd70228e26c66
CodingManoj <CodingManoj@gmail.com>
2024-09-08 02:29:38 -0400

    AutoPush

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ if [ "$pipeline" == "true" ] ;then
     exit 
 fi 
 
-ansible-playbook -i ${COMPONENT}-${ENV}.expense.internal, -e ansible_user=ec2-user -e ansible_password=${SSH_PSW} -e COMPONENT=${COMPONENT} -e VAULT_TOKEN=${VAULT_TOKEN}  -e ENV=${ENV} -e  expense.yml
+ansible-playbook -i ${COMPONENT}-${ENV}.expense.internal, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e COMPONENT=${COMPONENT} -e VAULT_TOKEN=${VAULT_TOKEN}  -e ENV=${ENV} -e  expense.yml
 
 echo "Printing Environment Variables To Detect Secrets If Any"
 env 
